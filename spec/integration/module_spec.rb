35a443ad4dbb5a1d88154614e34748fc672d1a8c
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2021-01-15 15:32:47 -0500

    testing

--- spec/integration/module_spec.rb
+++ spec/integration/module_spec.rb
@@ -4,13 +4,8 @@ RSpec.describe '`vtk module` command', type: :cli do
   it 'executes `vtk help module` command successfully' do
     output = `vtk help module`
     expected_output = <<~OUT
-Commands:
-  vtk module add <module name>         # Add a new module to vets-api
-  vtk module controller <module name>  # Add new controller to a module in vets-api
-  vtk module model <module name>       # Add new model to a module in vets-api
-  vtk module serializer <module name>  # Add new serializer to a module in vets-api
-  vtk module service <module name>     # Add new service class to a module in vets-api
-OUT
+      vtk module add <module name>         # Add a new module to vets-api
+    OUT
     expect(output).to include(expected_output)
   end
 end
