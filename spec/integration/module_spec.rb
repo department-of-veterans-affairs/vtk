433d43898c31c0c9749a0b328b627b50201622ae
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2021-01-15 15:54:10 -0500

    add controller spec

--- spec/integration/module_spec.rb
+++ spec/integration/module_spec.rb
@@ -9,34 +9,10 @@ RSpec.describe '`vtk module` command', type: :cli do
     expect(output).to include(expected_output)
   end
 
-  it 'executes `vtk help module and contains the controller command` command successfully' do
+  it 'executes `vtk help module` successfully and contains controller command' do
     output = `vtk help module`
     expected_output = <<~OUT
       vtk module controller <module name>  # Add new controller to a module in vets-api
-    OUT
-    expect(output).to include(expected_output)
-  end
-
-  it 'executes `vtk help module and contains the model command` command successfully' do
-    output = `vtk help module`
-    expected_output = <<~OUT
-      vtk module model <module name>       # Add new model to a module in vets-api
-    OUT
-    expect(output).to include(expected_output)
-  end
-
-  it 'executes `vtk help module and contains the serializer command` command successfull' do
-    output = `vtk help module`
-    expected_output = <<~OUT
-      vtk module serializer <module name>  # Add new serializer to a module in vets-api
-    OUT
-    expect(output).to include(expected_output)
-  end
-
-  it 'executes `vtk help module and contains the service command` command successfull' do
-    output = `vtk help module`
-    expected_output = <<~OUT
-      vtk module service <module name>     # Add new service class to a module in vets-api
     OUT
     expect(output).to include(expected_output)
   end
