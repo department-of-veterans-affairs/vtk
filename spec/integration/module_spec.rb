1c69d6b989ebd575eb54a23605be968fd1f84510
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2021-01-15 15:41:48 -0500

    define separate tests

--- spec/integration/module_spec.rb
+++ spec/integration/module_spec.rb
@@ -5,10 +5,6 @@ RSpec.describe '`vtk module` command', type: :cli do
     output = `vtk help module`
     expected_output = <<~OUT
       vtk module add <module name>         # Add a new module to vets-api
-      vtk module controller <module name>  # Add new controller to a module in vets-api
-      vtk module model <module name>       # Add new model to a module in vets-api
-      vtk module serializer <module name>  # Add new serializer to a module in vets-api
-      vtk module service <module name>     # Add new service class to a module in vets-api
     OUT
     expect(output).to include(expected_output)
   end
