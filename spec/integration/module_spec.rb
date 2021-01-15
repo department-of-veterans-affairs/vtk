a9f31c3d1ee6610c65dd25172214cb82b896ab40
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2021-01-15 14:29:42 -0500

    remove help command

--- spec/integration/module_spec.rb
+++ spec/integration/module_spec.rb
@@ -7,7 +7,6 @@ RSpec.describe '`vtk module` command', type: :cli do
       Commands:
         vtk module add <module name>         # Add a new module to vets-api
         vtk module controller <module name>  # Add new controller to a module in vets-api
-        vtk module help [COMMAND]            # Describe subcommands or one specific subcommand
         vtk module model <module name>       # Add new model to a module in vets-api
         vtk module serializer <module name>  # Add new serializer to a module in vets-api
         vtk module service <module name>     # Add new service class to a module in vets-api
