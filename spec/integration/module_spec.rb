ec851411f35b722381b8da7062da7acea79743b3
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2021-01-18 10:03:07 -0500

    test string matching

--- spec/integration/module_spec.rb
+++ spec/integration/module_spec.rb
@@ -9,18 +9,16 @@ RSpec.describe '`vtk module` command', type: :cli do
     expect(output).to include(expected_output)
   end
 
-  it 'executes `vtk help module` successfully and contains all commands' do
+  it 'executes `vtk help module` successfully and contains serializer command' do
+    output = `vtk help module`
+    expect(output).to include("vtk module serializer <module name>")
+  end
+
+  it 'executes `vtk help module` successfully and contains model command' do
     output = `vtk help module`
     expected_output = <<~OUT
-      Commands:
-        vtk module add <module name>         # Add a new module to vets-api
-        vtk module controller <module name>  # Add new controller to a module in vets-api
-        vtk module help [COMMAND]            # Describe subcommands or one specific subcommand
-        vtk module model <module name>       # Add new model to a module in vets-api
-        vtk module serializer <module name>  # Add new serializer to a module in vets-api
-        vtk module service <module name>     # Add new service class to a module in vets-api
+      vtk module model <module name>       # Add new model to a module in vets-api
     OUT
-
     expect(output).to include(expected_output)
   end
 end
