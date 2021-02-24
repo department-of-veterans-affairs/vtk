a1ff7b640dd4fe1ae86af2d8e16d26681e04088d
Travis Hilton <travis.hilton@oddball.io>
2021-02-24 12:01:55 -0800

    Change to fix failure of module_spec.

--- spec/integration/module_spec.rb
+++ spec/integration/module_spec.rb
@@ -1,33 +1,33 @@
 # frozen_string_literal: true
 
 RSpec.describe '`vtk module` command', type: :cli do
-  it 'executes `vtk help module` successfully and contains add command' do
-    output = `vtk help module`
+  it 'executes `vtk module help` successfully and contains add command' do
+    output = `vtk module help add`
     expect(output).to include('vtk module add')
-    expect(output).to include('# Add a new module')
+    expect(output).to include('Add a new module')
   end
 
-  it 'executes `vtk help module` successfully and contains controller command' do
-    output = `vtk help module`
+  it 'executes `vtk module help` successfully and contains controller command' do
+    output = `vtk module help controller`
     expect(output).to include('vtk module controller')
-    expect(output).to include('# Add new controller')
+    expect(output).to include('Add new controller')
   end
 
-  it 'executes `vtk help module` successfully and contains model command' do
-    output = `vtk help module`
+  it 'executes `vtk module help` successfully and contains model command' do
+    output = `vtk module help model`
     expect(output).to include('vtk module model')
-    expect(output).to include('# Add new model')
+    expect(output).to include('Add new model')
   end
 
-  it 'executes `vtk help module` successfully and contains serializer command' do
-    output = `vtk help module`
+  it 'executes `vtk module help` successfully and contains serializer command' do
+    output = `vtk module help serializer`
     expect(output).to include('vtk module serializer')
-    expect(output).to include('# Add new serializer')
+    expect(output).to include('Add new serializer')
   end
 
-  it 'executes `vtk help module` successfully and contains service command' do
-    output = `vtk help module`
+  it 'executes `vtk module help` successfully and contains service command' do
+    output = `vtk module help service`
     expect(output).to include('vtk module service')
-    expect(output).to include('# Add new service')
+    expect(output).to include('Add new service')
   end
 end
