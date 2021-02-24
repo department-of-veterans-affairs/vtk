b5b8e4fc6989d29d54cf8b403b08444b74a1e45e
Travis Hilton <travis.hilton@oddball.io>
2021-02-24 09:45:57 -0800

    Making small change to module_spec to account for truncated message.

--- spec/integration/module_spec.rb
+++ spec/integration/module_spec.rb
@@ -3,31 +3,31 @@
 RSpec.describe '`vtk module` command', type: :cli do
   it 'executes `vtk help module` successfully and contains add command' do
     output = `vtk help module`
-    expect(output).to include('vtk module add <module name>')
+    expect(output).to include('vtk module add')
     expect(output).to include('# Add a new module')
   end
 
   it 'executes `vtk help module` successfully and contains controller command' do
     output = `vtk help module`
-    expect(output).to include('vtk module controller <component name>')
+    expect(output).to include('vtk module controller')
     expect(output).to include('# Add new controller')
   end
 
   it 'executes `vtk help module` successfully and contains model command' do
     output = `vtk help module`
-    expect(output).to include('vtk module model <component name>')
+    expect(output).to include('vtk module model')
     expect(output).to include('# Add new model')
   end
 
   it 'executes `vtk help module` successfully and contains serializer command' do
     output = `vtk help module`
-    expect(output).to include('vtk module serializer <component name>')
+    expect(output).to include('vtk module serializer')
     expect(output).to include('# Add new serializer')
   end
 
   it 'executes `vtk help module` successfully and contains service command' do
     output = `vtk help module`
-    expect(output).to include('vtk module service <component name>')
+    expect(output).to include('vtk module service')
     expect(output).to include('# Add new service')
   end
 end
