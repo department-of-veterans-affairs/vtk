b3dff11929b709537986b9a823e486b69951cdc4
Rebecca Tolmach <10993987+rmtolmach@users.noreply.github.com>
2022-04-08 16:59:05 -0400

    bump version to 0.9.4

--- lib/vtk/commands/socks/setup.rb
+++ lib/vtk/commands/socks/setup.rb
@@ -120,7 +120,7 @@ module Vtk
         def access_request_template_url
           'https://github.com/department-of-veterans-affairs/va.gov-team/issues/new?' \
             'assignees=&labels=external-request%2C+operations%2C+ops-access-request&' \
-            'template=socks-access-request.yml&title=Access+for+%5Bindividual%5D'
+            'template=socks-access-request.yml&title=SOCKS+access+for+%5Bindividual%5D'
         end
 
         def copy_and_open_gh
